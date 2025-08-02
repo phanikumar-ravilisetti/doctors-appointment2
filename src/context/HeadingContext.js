9289906b4c77f6309f782a6fdbfcf297f5c84ac5
phanikumar-ravilisetti <kumarsfine1942@gmail.com>
2025-08-02 12:31:46 +0000

    doctors list

--- src/context/HeadingContext.js
+++
@@ -1,9 +0,0 @@
-import {createContext} from 'react'
-
-const HeadingContext = createContext({
-  heading:
-    'Hyper boost your Revenue Management, Marketing and Commercial Functions with Business Ready AI',
-  setHeading: () => {},
-})
-
-export default HeadingContext
